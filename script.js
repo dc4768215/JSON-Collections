2306ce18f5ad05d0736fc19aaa87e8e44b3a5f9b
Daphne Castro <dc4768@nyu.edu>
2022-04-07 10:59:31 -0400

    pay

--- script.js
+++ script.js
@@ -6,34 +6,34 @@ function printToPage(incoming) {
 
 let jsonDatabase = [
   {
-    "Name" = "Pim"
-    "pictureURL" = "https://static.wikia.nocookie.net/smiling-friends/images/f/fb/Pim.png/revision/latest?cb=20211223214250"
-    "Color" = "Bubble Gum Pink"
-    "Species" = "Critter"
-    "Occupation" = "Smiling Friend"
+    "Name" : "Pim",
+    "pictureURL" : "https://static.wikia.nocookie.net/smiling-friends/images/f/fb/Pim.png/revision/latest?cb=20211223214250",
+    "Color" : "Bubble Gum Pink",
+    "Species" : "Critter",
+    "Occupation" : "Smiling Friend"
   },
   {
-    "Name" = "Charlie"
-    "pictureURL" = "https://static.wikia.nocookie.net/smiling-friends/images/c/c9/Charlie.png/revision/latest?cb=20220113014948"
-    "Color" = "Piss Yellow"
-    "Species" = "Critter"
-    "Occupation" = "Smiling Friend"
+    "Name" : "Charlie",
+    "pictureURL" : "https://static.wikia.nocookie.net/smiling-friends/images/c/c9/Charlie.png/revision/latest?cb=20220113014948",
+    "Color" : "Piss Yellow",
+    "Species" : "Critter",
+    "Occupation" : "Smiling Friend"
   },
   {
-    "Name" = "The Boss"
-    "pictureURL" = "https://static.wikia.nocookie.net/smiling-friends/images/b/bf/The_boss.jpg/revision/latest/scale-to-width-down/700?cb=20220114223652"
-    "Color" = "Pale Peach"
-    "Species" = "Human"
-    "Occupation" = "Head of Smiling Friends Inc."
+    "Name" : "The Boss",
+    "pictureURL" : "https://static.wikia.nocookie.net/smiling-friends/images/b/bf/The_boss.jpg/revision/latest/scale-to-width-down/700?cb=20220114223652",
+    "Color" : "Pale Peach",
+    "Species" : "Human",
+    "Occupation" : "Head of Smiling Friends Inc."
   }
 ]
 
 for (var i = 0; i < jsonDatabase.length; i++) {
-  createElement(jsonDatabase[i]);
+  createElementProper(jsonDatabase[i]);
 }
 
 function createElementProper(incomingJSON) {
-  var divColor = #eefbfd;
+  var divColor = "#eefbfd";
 
   let newContentElement = document.createElement("DIV");
   newContentElement.style.backgroundColor = divColor;
@@ -49,17 +49,17 @@ function createElementProper(incomingJSON) {
   newImage.src = incomingJSON['pictureURL'];
   newContentElement.appendChild(newImage);
 
-  let newContentSubhead = document.createElement("H4");
-  newContentSubhead.innerText = "Color: " + incomingJSON['Color'];
-  newContentElement.appendChild(newContentSubhead);
+  let newColorSubhead = document.createElement("H4");
+  newColorSubhead.innerText = "Color: " + incomingJSON['Color'];
+  newContentElement.appendChild(newColorSubhead);
 
-  let newContentSubhead = document.createElement("H4");
-  newContentSubhead.innerText = "Species: " + incomingJSON['Species'];
-  newContentElement.appendChild(newContentSubhead);
+  let newSpeciesSubhead = document.createElement("H4");
+  newSpeciesSubhead.innerText = "Species: " + incomingJSON['Species'];
+  newContentElement.appendChild(newSpeciesSubhead);
 
-  let newContentSubhead = document.createElement("H4");
-  newContentSubhead.innerText = "Occupation: " + incomingJSON['Occupation'];
-  newContentElement.appendChild(newContentSubhead);
+  let newOccupSubhead = document.createElement("H4");
+  newOccupSubhead.innerText = "Occupation: " + incomingJSON['Occupation'];
+  newContentElement.appendChild(newOccupSubhead);
 
   contentGridElement.appendChild(newContentElement);
 }
